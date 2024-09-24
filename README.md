9c8cec4bef1c071b66ee0399e1ab8830e96c6879
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-09-24 09:00:06 +0200

    [EEE-22] Create the server

--- README.md
+++
@@ -1,1 +0,0 @@
-# serverER6
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+
+app.get('/players/email/:email', async (req, res) => {
+    const email = req.params.email;
+    const url = `https://kaotika-server.fly.dev/players/email/${email}`;
+    
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        console.error(`Error getting player data: ${error.message}`);
+        res.status(500).json({ error: 'Error getting player data' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
