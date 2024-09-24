29a96fc0db49f568bb52c422761f75beb90d49d0
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-09-24 10:04:47 +0200

    [EEE-50] moongose Linked with database and playerScheme created

---
+++ Schemas/PlayerSchema.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const modifierSchema = new Schema({
+  intelligence: { type: Number},
+  dexterity: { type: Number},
+  constitution: { type: Number},
+  insanity: { type: Number},
+  charisma: { type: Number},
+  strength: { type: Number},
+  hit_points: { type: Number} 
+}, { _id: false });
+
+const equipmentSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  name: { type: String},
+  description: { type: String },
+  type: { type: String },
+  image: { type: String },
+  value: { type: Number},
+  min_lvl: { type: Number},
+  damage: { type: String },              
+  base_percentage: { type: Number },      
+  defense: { type: Number },          
+  duration: { type: Number },               
+  modifiers: modifierSchema
+}, { _id: false });
+
+const recoveryEffectSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  name: { type: String},
+  description: { type: String },
+  type: { type: String },
+  modifiers: modifierSchema
+}, { _id: false });
+
+const antidotePotionSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  name: { type: String},
+  description: { type: String },
+  type: { type: String },
+  image: { type: String },
+  value: { type: Number},
+  min_lvl: { type: Number},
+  recovery_effect: recoveryEffectSchema
+}, { _id: false });
+
+const profileAttributeSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  name: { type: String },
+  description: { type: String },
+  value: { type: Number }
+}, { _id: false });
+
+const profileSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  name: { type: String},
+  description: { type: String },
+  image: { type: String },
+  attributes: [profileAttributeSchema]
+}, { _id: false });
+
+const taskSchema = new Schema({
+  classroomId: { type: String },
+  courseWorkName: { type: String },
+  grade: { type: Number },
+  selectedAssignment: { type: String },
+  _id: mongoose.Types.ObjectId
+}, { _id: false });
+
+const inventorySchema = new Schema({
+  helmets: [equipmentSchema],
+  weapons: [equipmentSchema],
+  armors: [equipmentSchema],
+  shields: [equipmentSchema],
+  artifacts: [equipmentSchema],
+  boots: [equipmentSchema],
+  rings: [equipmentSchema],
+  antidote_potions: [antidotePotionSchema],
+  healing_potions: [equipmentSchema],
+  enhancer_potions: [equipmentSchema]
+}, { _id: false });
+
+const playerSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  name: { type: String},
+  nickname: { type: String },
+  email: { type: String },
+  classroom_Id: { type: String },
+  level: { type: Number},
+  experience: { type: Number},
+  is_active: { type: Boolean},
+  avatar: { type: String },
+  created_date: { type: Date},
+  gold: { type: Number},
+  attributes: modifierSchema,
+  equipment: {
+    weapon: equipmentSchema,
+    armor: equipmentSchema,
+    artifact: equipmentSchema,
+    antidote_potion: antidotePotionSchema,
+    healing_potion: equipmentSchema,
+    enhancer_potion: equipmentSchema,
+    helmet: equipmentSchema,
+    shield: equipmentSchema,
+    boot: equipmentSchema,
+    ring: equipmentSchema
+  },
+  inventory: inventorySchema,
+  profile: profileSchema,
+  tasks: [taskSchema]
+});
+
+module.exports = mongoose.model('Player', playerSchema);
--- index.js
+++ index.js
@@ -19,3 +19,25 @@ app.get('/players/email/:email', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+const mongoose = require('mongoose');
+
+//mongodb+srv://larrea:3@cluster.t0rfr.mongodb.net/
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://larrea:3@cluster.t0rfr.mongodb.net/');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const kittySchema = new mongoose.Schema({
+    name: String
+  });
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
