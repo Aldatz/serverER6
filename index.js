ed7d5f1e8a068754f19b347bab433816b8d00764
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-10-15 09:45:32 +0200

    [EEE-132] add request to potions api in server

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ app.get('/mortimer', async (req, res) => {
 });
 
 // Cambia app.post a app.get
-app.get('/get-ingredients', async (req, res) => {
+app.get('/ingredients', async (req, res) => {
   try {
     const url = `https://kaotika-server.fly.dev/ingredients`;
     const response = await axios.get(url);
@@ -260,6 +260,33 @@ app.get('/get-ingredients', async (req, res) => {
     res.status(500).json({
       success: false,
       message: 'Failed to fetch ingredients',
+      error: error.message 
+    });
+  }
+});
+
+app.get('/potions', async (req, res) => {
+  try {
+    const url = `https://kaotika-server.fly.dev/diseases`;
+    const response = await axios.get(url);
+    
+    // Verifica si la respuesta tiene datos
+    if (response.data && response.data.data) {
+      res.json({
+        success: true,
+        potionsData: response.data.data,  // Enviar el array de ingredientes
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'No potions found',
+      });
+    }
+  } catch (error) {
+    console.error('Error fetching potions:', error.message);
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch potions',
       error: error.message 
     });
   }
