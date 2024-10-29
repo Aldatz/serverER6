bfe36fb401071105ddfe13c3f33eaa6577ded3a9
Sergio Galdos <sgaldos@MacBook-Pro-de-Sergio.local>
2024-10-29 10:53:59 +0100

    [MAIN][TEST] test pm2

--- index.js
+++ index.js
@@ -442,7 +442,7 @@ mqttClient.on('message', async (topic, message) => {
   }
 });
 
-console.log("self-deployment test Sergio_6");
+console.log("self-deployment test Sergio_7");
 
 // Manejar mensajes recibidos en el tópico 'EIASidCard'
 mqttClient.on('message', (topic, message) => {
