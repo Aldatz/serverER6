8b76a56d94c36d6019e46feb2b92c6928441fee0
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-10-30 10:04:21 +0100

    [main][FIX] deletions when adding new code

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ app.get('/mortimer', async (req, res) => {
 });
 
 // Cambia app.post a app.get
-app.get('/get-ingredients', async (req, res) => {
+app.get('/ingredients', async (req, res) => {
   try {
     const url = `https://kaotika-server.fly.dev/ingredients`;
     const response = await axios.get(url);
@@ -270,6 +270,35 @@ app.get('/get-ingredients', async (req, res) => {
     });
   }
 });
+
+app.get('/potions', async (req, res) => {
+  try {
+    const url = `https://kaotika-server.fly.dev/diseases`;
+    const response = await axios.get(url);
+    
+    // Verifica si la respuesta tiene datos
+    if (response.data && response.data.data) {
+      res.json({
+        success: true,
+        potionsData: response.data.data,  // Enviar el array de ingredientes
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'No potions found',
+      });
+    }
+  } catch (error) {
+    console.error('Error fetching potions:', error.message);
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch potions',
+      error: error.message 
+    });
+  }
+});
+
+// Ruta para verificar token
 
 // Ruta para verificar token
 app.post('/verify-token', async (req, res) => {
