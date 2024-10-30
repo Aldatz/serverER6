ce12bce5825d5406bef58c2f3be297bbd4d6c400
Sergio <galdos.sergi@outlook.com>
2024-10-30 08:26:34 +0100

    [MAIN][TEST] test pm2 auto-deploy

--- index.js
+++ index.js
@@ -458,6 +458,7 @@ mqttClient.on('message', async (topic, message) => {
 });
 
 console.log("self-deployment test Sergio_17");
+console.log("self-deployment test Sergio_18");
 
 // Manejar mensajes recibidos en el tópico 'EIASidCard'
 mqttClient.on('message', (topic, message) => {
