c408efc26ad5b9d78d7cc2d42ed39f127862dae3
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-11-13 13:37:05 +0100

    [EEE-236][ADD] adds avatar to socket

--- server.js
+++ server.js
@@ -139,8 +139,8 @@ io.on('connection', async (socket) => {
   });
   //location data from a device
   socket.on('locationUpdate', (data) => {
-  const { userId, location } = data;
-  deviceLocations[userId] = location; //save device location by user ID
+  const { userId, location, avatar } = data;
+  deviceLocations[userId] = {location, avatar}; //save device location by user ID
 
   //broadcast location to all clients
   io.emit('deviceLocations', deviceLocations);
