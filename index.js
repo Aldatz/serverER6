dd6bfd2e7dc7d839a6596321054c1b976783a6ef
LarreaE <eneko.larrea@ikasle.aeg.eus>
2025-01-09 11:52:35 +0100

    [EEE-400][DELETE] deleted duplicate insertPlayerfunction

--- index.js
+++ index.js
@@ -442,53 +442,6 @@ server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-// Función para insertar jugador en la base de datos
-async function insertPlayer(playerData) {
-  try {
-      const data = playerData.data;
-      const existingPlayer = await Player.findOne({ email: data.email });
-
-      console.log(existingPlayer);
-      
-      if (existingPlayer) {
-          // Actualiza los campos necesarios sin cambiar el estado de is_active
-          await Player.updateOne(
-            { email: data.email },
-            {
-              ...data,
-              is_active: existingPlayer.is_active, // Manten el estado existente
-              is_inside_tower: existingPlayer.is_inside_tower,
-              "inventory.ingredients": existingPlayer.inventory.ingredients, //manten ingredientes
-            }
-          );
-          console.log(`Player with email ${data.email} updated successfully.`);
-          return existingPlayer;
-      } else {
-          data.is_active = false; // Solo para nuevos jugadores
-          data.is_inside_tower = false;
-      switch (data.email) {
-        case process.env.ISTVAN_EMAIL:
-          data.role = 'ISTVAN';
-          break;
-        case process.env.VILLAIN_EMAIL:
-          data.role = 'VILLAIN';
-          break;
-        case process.env.MORTIMER_EMAIL:
-          data.role = 'MORTIMER';
-          break;
-        default:
-          data.role = 'ACOLYTE';
-          break;
-      }
-      const newPlayer = new Player(data);
-      await newPlayer.save();
-      console.log(`Player with email ${data.email} created successfully.`);
-      return newPlayer;
-    }
-  } catch (error) {
-    console.error('Error updating/creating player:', error);
-  }
-}
 
 mqttClient.on('error', (error) => {
   console.error('Error al conectar al broker MQTT:', error);
--- services/authService.js
+++ services/authService.js
@@ -6,40 +6,44 @@ export const insertPlayer = async (playerData) => {
     const data = playerData.data;
     const existingPlayer = await Player.findOne({ email: data.email });
 
+    console.log(existingPlayer);
+    
     if (existingPlayer) {
-      await Player.updateOne(
-        { email: data.email },
-        {
-          ...data,
-          is_active: existingPlayer.is_active,
-          is_inside_tower: existingPlayer.is_inside_tower,
-        }
-      );
-      console.log(`Player with email ${data.email} updated successfully.`);
-      return existingPlayer;
+        // Actualiza los campos necesarios sin cambiar el estado de is_active
+        await Player.updateOne(
+          { email: data.email },
+          {
+            ...data,
+            is_active: existingPlayer.is_active, // Manten el estado existente
+            is_inside_tower: existingPlayer.is_inside_tower,
+            "inventory.ingredients": existingPlayer.inventory.ingredients, //manten ingredientes
+          }
+        );
+        console.log(`Player with email ${data.email} updated successfully.`);
+        return existingPlayer;
     } else {
-      data.is_active = false;
-      data.is_inside_tower = false;
-      switch (data.email) {
-        case process.env.ISTVAN_EMAIL:
-          data.role = 'ISTVAN';
-          break;
-        case process.env.VILLAIN_EMAIL:
-          data.role = 'VILLAIN';
-          break;
-        case process.env.MORTIMER_EMAIL:
-          data.role = 'MORTIMER';
-          break;
-        default:
-          data.role = 'ACOLYTE';
-          break;
-      }
-      const newPlayer = new Player(data);
-      await newPlayer.save();
-      console.log(`Player with email ${data.email} created successfully.`);
-      return newPlayer;
+        data.is_active = false; // Solo para nuevos jugadores
+        data.is_inside_tower = false;
+    switch (data.email) {
+      case process.env.ISTVAN_EMAIL:
+        data.role = 'ISTVAN';
+        break;
+      case process.env.VILLAIN_EMAIL:
+        data.role = 'VILLAIN';
+        break;
+      case process.env.MORTIMER_EMAIL:
+        data.role = 'MORTIMER';
+        break;
+      default:
+        data.role = 'ACOLYTE';
+        break;
     }
-  } catch (error) {
-    console.error('Error updating/creating player:', error);
+    const newPlayer = new Player(data);
+    await newPlayer.save();
+    console.log(`Player with email ${data.email} created successfully.`);
+    return newPlayer;
   }
+} catch (error) {
+  console.error('Error updating/creating player:', error);
+}
 };
