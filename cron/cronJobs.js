425851b57fc4769e55d8cef7c10087025589b952
ionpazoos <jon.pazos@ikasle.aeg.eus>
2025-01-12 13:45:26 +0100

    [UPADTE]changed cron to 30 mins

--- cron/cronJobs.js
+++ cron/cronJobs.js
@@ -7,9 +7,9 @@ export function scheduleCronJobs() {
   // Tarea 1: Cada 30 minutos → Reducir la resistencia en 10%
   // Formato CRON -> '*/30 * * * *' = cada 30 min
   // (Sintaxis: minuto hora diaDelMes mes diaDeLaSemana)
-  //cron.schedule('*/30 * * * *', async () => { 
+  cron.schedule('*/30 * * * *', async () => { 
     //(si quieres hacer pruebas con cron usar este)
-     cron.schedule('* * * * * *', async () => {
+     //cron.schedule('* * * * * *', async () => {
     console.log('[CRON] Reducing Resistance by 10% for all players...');
     await reduceResistance();
     // Cualquier otra lógica
